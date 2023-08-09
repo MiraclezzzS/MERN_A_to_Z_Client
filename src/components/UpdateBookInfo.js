5b5a447685a4b0d4547d65e12e40ccdfd10b64e6
MiraclezzzS <131764763+MiraclezzzS@users.noreply.github.com>
2023-08-10 06:48:44 +0800

    Update UpdateBookInfo.js

--- src/components/UpdateBookInfo.js
+++ src/components/UpdateBookInfo.js
@@ -18,7 +18,7 @@ function UpdateBookInfo(props) {
 
   useEffect(() => {
     axios
-      .get(`https://mern-a-to-z.vercel.app/api/books/${id}`)
+      .get(`https://mern-a-to-z.vercel.app/${id}`)
       .then((res) => {
         setBook({
           title: res.data.title,
@@ -51,7 +51,7 @@ function UpdateBookInfo(props) {
     };
 
     axios
-      .put(`https://mern-a-to-z.vercel.app/api/books/${id}`, data)
+      .put(`https://mern-a-to-z.vercel.app/${id}`, data)
       .then((res) => {
         navigate(`/show-book/${id}`);
       })
